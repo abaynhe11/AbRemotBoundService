6c74841e8a9ca426f8ee503a8fd5451ec07c5197
Abaynhe Abitew <abaynheabitew@yahoo.com>
2022-09-23 20:09:43 +0300

    An example of remote bound service (message/handler).

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/abremotboundservice/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.google.firebase.quickstart.auth.abremotboundservice
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    var myService: Messenger? = null
+    var isBound: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(getApplicationContext(), RemoteService::class.java)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE)
+    }
+
+        fun sendMessage(view: View) {
+            if (!isBound) return
+
+            val msg = Message.obtain()
+
+            val bundle = Bundle()
+            bundle.putString("MyString", "Message Received")
+
+            msg.data = bundle
+
+            try {
+                myService?.send(msg)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+
+        }
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(
+            className: ComponentName,
+            service: IBinder
+        ) {
+            myService = Messenger(service)
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(
+            className: ComponentName) {
+            myService = null
+            isBound = false
+        }
+    }
+
+
+
+    }
